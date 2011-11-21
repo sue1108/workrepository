98d4d5a572f1204e2bc24464d038d662a5d3a0e6
suyx <sue1108@live.cn>
2011-11-21 17:33:34 +0800

    存储静态变量

---
+++ com.succez.study.webserver/src/main/java/com/succez/study/webserver/common/StaticConstant.java
@@ -0,0 +1,24 @@
+package com.succez.study.webserver.common;
+
+/**
+ * 此类用来存放全局的静态常量
+ * @author sue
+ * @createdate 2011-11-9
+ */
+public class StaticConstant {
+
+	/**
+	 * 初始化BufferReader对象默认的存储空间
+	 */
+	public static int BUFFERREADER_LEN = 8;
+
+	/**
+	 * 创建List对象的时候初始容量为5
+	 */
+	public static int LIST_LEN = 5;
+
+	/**
+	 * 初始化StringBuffer对象默认的存储空间
+	 */
+	public static int STRINGBUFFER_LEN = 8;
+}
